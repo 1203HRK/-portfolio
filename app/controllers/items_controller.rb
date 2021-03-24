5b2a5dc1e5146b9932a23e0b4bc5847640f7e627
1203HRK <14haaru05@gmail.com>
2021-03-24 13:14:33 +0000

    add[item-show]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :autheniticate_user, {only: [:edit, :update, :destroy]}
-  
+
   def new
     @item = Item.new
   end
@@ -20,9 +20,28 @@ class ItemsController < ApplicationController
     @reviews = @item.reviews
     @item_ranks = Review.create_item_ranks(@item.id)
     @average = @item.reviews.average(:rate).to_f.round(1)
-    personal = Personal.all
-     # パーソナルに紐づいたレビュー
-    @personal_reviews = Review.joins(user: :personal_users).where(user: { personal_users: { personal: personal }})
+    @personals = Personal.all
+
+    # show page users
+    user_ids = @reviews.pluck('user_id')
+    personal_user_ids = PersonalUser.where(user_id: user_ids.uniq).pluck('personal_id')
+    @personal = Personal.where(id: personal_user_ids)
+    @personal_all_users = {}
+    # @personal 'イエローベース', ブルーベース,,,etc
+    @personal.each do | personal |
+      @personal_all_users[personal.name] = PersonalUser.where(personal_id: Personal.where(name: personal.name).pluck('id')).pluck('user_id')
+    end
+    #personal_all_users {"ブルーベース"=>[1, 2, 3], "spring"=>[1, 2, 3]}
+    #hash[key] = value
+    #{key => value}
+    @personal_and_review_counts = {}
+    # user_ids [5,5,5]
+    user_ids.each.with_index(1) do | id, index |
+      @personal_all_users.each do |personal_name, user_ids|
+        @personal_and_review_counts[personal_name] = 0 if index == 1
+        @personal_and_review_counts[personal_name] += user_ids.count(id)
+      end
+    end
   end
 
   def edit
