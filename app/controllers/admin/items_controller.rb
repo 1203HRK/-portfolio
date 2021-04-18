da9ff1eea99ac09a6ec961c57e208cec9e0d1311
1203HRK <14haaru05@gmail.com>
2021-04-18 04:33:56 +0000

    add[admin-itemshow]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,6 +2,11 @@ class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @reviews = @item.reviews
+  end
 
   def edit; end
 end
