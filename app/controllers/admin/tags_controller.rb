51e47ee19cc1c24280ed9a32896c5a23b93f6f11
1203HRK <14haaru05@gmail.com>
2021-04-20 10:49:02 +0000

    add[admin-tagedit]

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,5 +3,17 @@ class Admin::TagsController < ApplicationController
     @tags = Tag.all
   end
 
-  def edit; end
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+  
+  def update
+    tag = Tag.find(params[:id])
+    if tag.save
+      redirect_to admin_tags_path
+    else
+      render :edit
+    end
+  end
+  
 end
